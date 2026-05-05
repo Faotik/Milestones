313ab804d3b83e9fb21dc83e9605cfcd0f9957d9
Faotik <faotikmail@gmail.com>
2026-05-05 18:17:03 +0100

    Implement tabs

--- src/main/java/ModName/GUI/GUIFactoryMilestones.java
+++ src/main/java/ModName/GUI/GUIFactoryMilestones.java
@@ -3,12 +3,20 @@ package ModName.GUI;
 import ModName.Configs.ConfigMilestones;
 import ModName.ModName;
 import com.cleanroommc.modularui.api.UIFactory;
+import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.drawable.ItemDrawable;
 import com.cleanroommc.modularui.factory.GuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.utils.Alignment;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widget.EmptyWidget;
+import com.cleanroommc.modularui.widget.Widget;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.ItemDisplayWidget;
+import com.cleanroommc.modularui.widgets.ListWidget;
 import com.cleanroommc.modularui.widgets.TextWidget;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.entity.player.EntityPlayer;
@@ -19,6 +27,7 @@ import net.minecraft.util.EnumChatFormatting;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,47 +50,141 @@ public class GUIFactoryMilestones implements UIFactory<GUIDataMilestones> {
         final int columnCount = 4;
         final int columnWidth = (panelWidth - columnOffset * 2) / columnCount;
         final int rowHeight = 24;
-        final int textOffsetX = 20;
+        final int textOffsetX = 21;
         final int textOffsetY = 4;
+        final int sectionTitleOffsetX = 0;
+        final int sectionTitleOffsetY = 8;
+        final int tabTitleOffsetY = 10;
+        final int tabOffsetX = 4;
+        final int tabOffsetY = -21;
+        final int tabOffsetBetween = 8;
+        final int tabSize = 22;
+        final int tabIconSize = 18;
+        final int tabPadding = (22 - tabIconSize) / 2;
+        final int milestoneWidth = columnWidth - 10;
+        final int milestoneHeight = 22;
+        final int milestonePadding = 2;
 
         int columnIndex = 0;
         int rowIndex = 0;
+        int tabIndex = 0;
 
         ModularPanel panel = new ModularPanel("milestonesgui")
             .size(panelWidth, panelHeight);
 
         List<String> allMilestones = Arrays.asList(ConfigMilestones.milestones.items);
 
+        List<ListWidget> tabPanels = new ArrayList<>();
+//        List<ButtonWidget> tabButtons = new ArrayList<>();
+
+        panel.child(
+            ButtonWidget.panelCloseButton()
+        );
+
+        ListWidget tab = null;
+
         for (var milestone : allMilestones) {
-            String[] parts = milestone.split(":");
-            String modid = parts[0];
-            String name = parts[1];
-            int meta = parts.length > 2 ? Integer.parseInt(parts[2]) : 0;
+            if (milestone.charAt(0) == '$') {
+                if (tab != null) {
+                    tab.disabled();
+                    tabPanels.add(tab);
+                    panel.child(tab);
+                    columnIndex = 0;
+                    rowIndex = 0;
+                }
+                tab = new ListWidget<>()
+                    .full();
 
-            Item item = GameRegistry.findItem(modid, name);
-            ItemStack stack = new ItemStack(item, 1, meta);
-            
-            panel.child(
-                new ItemDisplayWidget()
-                    .item(stack)
-                    .pos(columnIndex * columnWidth + columnOffset, rowIndex * rowHeight + rowOffset)
-            );
+                String[] parts = milestone.split(",");
+                String[] partsItem = parts[1].split(":");
+                String title = parts[0].substring(1);
+                String modid = partsItem[0];
+                String name = partsItem[1];
+                int meta = partsItem.length > 2 ? Integer.parseInt(partsItem[2]) : 0;
 
-            String time = "Incomplete";
-            if (guiData.completedMilestones != null && guiData.completedMilestones.hasKey(milestone)) {
-                time = EnumChatFormatting.GREEN + getTotalWorldTimeString(guiData.completedMilestones.getLong(milestone));
-            }
-            panel.child(
-                new TextWidget<>(time)
-                    .pos(columnIndex * columnWidth + columnOffset + textOffsetX, rowIndex * rowHeight + rowOffset + textOffsetY)
-            );
+                Item item = GameRegistry.findItem(modid, name);
+                ItemStack stack = new ItemStack(item, 1, meta);
 
-            columnIndex++;
-            if (columnIndex > columnCount - 1) {
+                int offsetX = tabIndex == 0 ? tabOffsetX : tabOffsetBetween;
+                final int finalTabIndex = tabIndex;
+                panel.child(
+                    new ButtonWidget<>()
+                        .pos(tabIndex * tabSize + offsetX, tabOffsetY)
+                        .size(tabSize)
+                        .padding(tabPadding)
+                        .overlay(new ItemDrawable(stack))
+                        .onMousePressed(mouseButton -> {
+                            System.out.println(finalTabIndex);
+                            if (mouseButton == 0 || mouseButton == 1) {
+                                for (int i = 0; i < tabPanels.size(); i++) {
+                                    tabPanels.get(i).setEnabled(i == finalTabIndex);
+                                }
+                                return true;
+                            }
+                            return false;
+                        })
+                );
+                tabIndex++;
+
+                tab.child(
+                    new TextWidget<>(title)
+                        .posRel(Alignment.TopCenter)
+                        .marginTop(tabTitleOffsetY)
+                );
+            } else if (milestone.charAt(0) == '^') {
                 columnIndex = 0;
+                if(rowIndex != 0) {
+                    rowIndex++;
+                }
+
+                tab.child(
+                    new TextWidget<>(milestone.substring(1))
+                        .pos(columnOffset + sectionTitleOffsetX, rowIndex * rowHeight + rowOffset + sectionTitleOffsetY)
+                );
+
                 rowIndex++;
             }
+            else {
+                String[] parts = milestone.split(":");
+                String modid = parts[0];
+                String name = parts[1];
+                int meta = parts.length > 2 ? Integer.parseInt(parts[2]) : 0;
+
+                Item item = GameRegistry.findItem(modid, name);
+                ItemStack stack = new ItemStack(item, 1, meta);
+
+                String timeText = "Incomplete";
+                if (guiData.completedMilestones != null && guiData.completedMilestones.hasKey(milestone)) {
+                    timeText = EnumChatFormatting.GREEN + getTotalWorldTimeString(guiData.completedMilestones.getLong(milestone));
+                }
+
+                tab.child(
+                    new ListWidget<>()
+                        .pos(columnIndex * columnWidth + columnOffset, rowIndex * rowHeight + rowOffset)
+                        .size(milestoneWidth, milestoneHeight)
+                        .background(GuiTextures.MC_BUTTON)
+                        .child(
+                            new ItemDisplayWidget()
+                                .item(stack)
+                                .pos(milestonePadding, milestonePadding)
+                        )
+                        .child(
+                            new TextWidget<>(timeText)
+                                .pos(textOffsetX + milestonePadding, textOffsetY + milestonePadding)
+                        )
+                );
+
+                columnIndex++;
+                if (columnIndex > columnCount - 1) {
+                    columnIndex = 0;
+                    rowIndex++;
+                }
+            }
         }
+        tab.disabled();
+        tabPanels.add(tab);
+        panel.child(tab);
+        tabPanels.get(0).setEnabled(true);
 
         return panel;
     }
